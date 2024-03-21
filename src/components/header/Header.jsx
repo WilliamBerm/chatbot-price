3dc282db9ac95f97757bd9856d74f1839d130ebd
WilliamBerm <wabq301@gmail.com>
2024-03-21 15:35:49 -0500

    home update with jump

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./header.css"
-import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
   return (
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
 import "./home.css"
 
+      window.addEventListener('mouseover', initLandbot, { once: true });
+      window.addEventListener('touchstart', initLandbot, { once: true });
+      var myLandbot;
+      function initLandbot() {
+        if (!myLandbot) {
+          var s = document.createElement('script');s.type = 'text/javascript';s.async = true;
+          s.addEventListener('load', function() {
+            var myLandbot = new Landbot.Livechat({
+              configUrl: 'https://storage.googleapis.com/landbot.site/v3/H-2164207-UQ51GMH38BJ8CPPI/index.json',
+            });
+          });
+          s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
+          var x = document.getElementsByTagName('script')[0];
+          x.parentNode.insertBefore(s, x);
+        }
+      }
+
 const Home = () => {
   return (
     <div>
-        <section class="py-5 text-center container">
-        <div class="jumbotron">
-            <h1 class="display-4 text-start fw-bold">¡Cotiza rápido, con estilo personalizado
-            y online!</h1>
-            <p class="lead text-start">Cotibot es la solución a la hora de cotizar la remodelación de tu vivienda en obra gris desde casa y en segundos. Se adapta a tu presupuesto y ofrece dos catálogos de diseño imperdibles. ¿Te animas a probarlo?</p>
-            <hr class="my-4"/>
-            <button class="btn btn-outline-dark btn-lg rounded-pill"><a href='https://landbot.site/v3/H-2164207-UQ51GMH38BJ8CPPI/index.html'>Quiero cotizar</a></button>
+    <section class="py-5 text -center container">
+    <div class="jumbotron d-flex align-items-center justify-content-center flex-column flex-md-row">
+        <div class="text-md-start">
+            <h1 class="display-4 fw-bold">¡Cotiza rápido, con estilo personalizado y online!</h1>
+            <p class="lead p-2">Cotibot es la solución a la hora de cotizar la remodelación de tu vivienda en obra gris desde casa y en segundos. Se adapta a tu presupuesto y ofrece dos catálogos de diseño imperdibles. ¿Te animas a probarlo?</p>
+            <button class="btn btn-outline-dark btn-lg rounded-pill"><a href='https://landbot.site/v3/H-2164207-UQ51GMH38BJ8CPPI/index.html'>¡Quiero cotizar!</a></button>
         </div>
-        </section>
-     
-    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+        <img src="../src/assets/large-14.jpg" alt="Italian Trulli" width="500" height="400" class="ms-md-3 mt-3 mt-md-0 p-2 rounded-end-5"></img>
+    </div>
+</section>
+
+<div class="d-flex tittle-catalog">
+<h2 class="float-md-start mb-0 fw-bold p-4">Catalogo</h2>
+</div>
+    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center"> 
       <div class="col">
         <div class="card mb-4 rounded-3 shadow-sm">
           <div class="card-header py-3">
@@ -67,23 +88,6 @@ const Home = () => {
         </div>
       </div>
     </div>
-    <div class="container">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p class="col-md-4 mb-0 text-body-secondary">© 2024 Company, Inc</p>
-
-    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-    </a>
-
-    <ul class="nav col-md-4 justify-content-end">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
-    </ul>
-  </footer>
-</div>
     </div>
   )
 }
