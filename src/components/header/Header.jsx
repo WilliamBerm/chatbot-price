d562fc07f19f8e902c6f3a2f67ae9c20b19e96db
WilliamBerm <wabq301@gmail.com>
2024-03-23 12:51:15 -0500

    bot updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
   return (
     <header class="mb-auto">
     <div>
-      <h3 class="float-md-start mb-0">Company</h3>
+      <h3 class="float-md-start mb-0">HouseDesigns</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Contact</a>
+        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        <a class="nav-link" href="#">Tienda</a>
+        <a class="nav-link" href="#">Contacto</a>
       </nav>
     </div>
   </header>
